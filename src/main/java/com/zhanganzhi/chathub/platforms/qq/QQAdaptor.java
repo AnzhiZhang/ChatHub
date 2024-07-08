c4448b54717481809cd9feb055daa4a0610bf521
Aimerny <bbbxingyu@gmail.com>
2024-07-08 21:05:49 +0800

    Update QQAdaptor.java

--- src/main/java/com/zhanganzhi/chathub/platforms/qq/QQAdaptor.java
+++ src/main/java/com/zhanganzhi/chathub/platforms/qq/QQAdaptor.java
@@ -75,7 +75,6 @@ public class QQAdaptor extends AbstractAdaptor<QQFormatter> {
             ) {
                 JSONArray message = curEvent.getMessage();
                 List<String> messages = new ArrayList<>();
-                chatHub.getLogger().info("Event:{}",curEvent);
                 for (int i = 0; i < message.size(); i++) {
                     JSONObject part = message.getJSONObject(i);
                     if (part.getString("type").equals("text")) {
